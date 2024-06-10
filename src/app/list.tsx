d5a7d722479c7bb8dcaf9e7b41f666d837e56d76
Jason Keo <jasonkeo047@outlook.com>
2024-06-10 18:02:02 +0800

    s

--- src/app/list.tsx
+++ src/app/list.tsx
@@ -7,41 +7,43 @@ import Items from './items';
 export default function List() {
 
   let [todoitem, setitems] = useState<string[]>([]);
-  
 
-//s
+
+  //s
   function additem(event: React.FormEvent) {
     event.preventDefault();
     ;
-    if (document.getElementById('form') && document.getElementById('form').value === '') {
-      return;
-    }
-  
     if (document.getElementById('form')) {
+
+      if (document.getElementById('form').value === '') {
+        return;
+      }
+
+
       setitems([...todoitem, document.getElementById('form').value]);
       document.getElementById('form').value = '';
     }
   }
 
-  
-  
+
+
 
   return (
 
     <div className="">
-      
+
       <form className="mx-auto bg-grey-500" onSubmit={additem}>
 
 
         <input placeholder="Add task" id="form" className="rounded-full border-2 border-black-500  px-2 py-2 mx-2" type="text" />
-        
+
 
       </form>
-      
-      
-    <div className="my-4">
-    <Items listofitems={todoitem} setListofitems={setitems}/>
-    </div>
+
+
+      <div className="my-4">
+        <Items listofitems={todoitem} setListofitems={setitems} />
+      </div>
     </div>
 
 
