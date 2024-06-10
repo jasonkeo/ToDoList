7b67ed65392d20ef34be81d3f4387c730d051a87
Jason Keo <jasonkeo047@outlook.com>
2024-06-10 17:17:18 +0800

    intial commit

---
+++ src/app/items.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Items({ listofitems, setListofitems }) {
+  function remove(index){
+    const newList = [...listofitems];
+    newList.splice(index, 1);
+    setListofitems(newList);
+  }
+
+  if (listofitems) {
+    return (
+      <div>
+      {listofitems.map((item, index) => (
+        <div className="border-2 border-black-500 px-2 py-2 flex items-center justify-between " key={index}>
+          {index + 1}. {item} 
+          <button onClick={() => remove(index)} className="bg-red-600 hover:bg-red-700 text-white text-sm px-2 py-2 border rounded-full"></button>
+        </div>
+      ))}
+    </div>
+
+    );
+  } else {
+    return null;
+  }
+}
---
+++ src/app/list.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState } from "react";
+import Items from './items';
+
+
+export default function List() {
+
+  let [todoitem, setitems] = useState<List>([]);
+  
+
+
+  function additem(event) {
+    event.preventDefault();
+    if (document.getElementById('form').value == '') {
+      return;
+    }
+    
+    
+
+    setitems([...todoitem, document.getElementById('form').value]);
+
+    document.getElementById('form').value = '';
+
+  }
+
+  
+  
+
+  return (
+
+    <div className="">
+      
+      <form onSubmit="return false" className="mx-auto bg-grey-500" onSubmit={additem}>
+
+
+        <input placeholder="Add task" id="form" className="border-2 border-black-500  px-2 py-2 mx-2" type="text" />
+        
+
+      </form>
+      
+      
+    <div className="my-4">
+    <Items listofitems={todoitem} setListofitems={setitems}/>
+    </div>
+    </div>
+
+
+  );
+}
+
+
