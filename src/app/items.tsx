5331f92c0176aabef17afdfec5ac4fffc8c1685e
Jason Keo <jasonkeo047@outlook.com>
2024-06-18 16:32:55 +0800

    dawe

--- src/app/items.tsx
+++ src/app/items.tsx
@@ -17,11 +17,11 @@ export default function Items({ listofitems, setListofitems }: ItemsProps) {
 
   if (listofitems) {
     return (
-      <div style={{ width: '60vw',height: '70vh',}} className="border-4 border-black-900 overflow-scroll">
+      <div style={{ minWidth: '300px',width: '60vw',height: '70vh'}} className="border-4 border-black-100 overflow-scroll">
         {listofitems.map((item, index) => (
         <div className="m-2 border-2 border-black-500 px-2 py-2 flex items-center justify-between" key={index}>
       
-          <div style={{  width: '55vw', overflowWrap: 'break-word' }} className="">{index + 1}. {item} </div>
+          <div style={{  width: '85%', overflowWrap: 'break-word' }} className="">{index + 1}. {item} </div>
           
           <button onClick={() => remove(index)} className="text-red-500 text-3xl p-0 mx-2 rounded-full"> &times; </button>
           
